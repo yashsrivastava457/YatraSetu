b916dd7b54845f974cc473bf3ba223bf69808def
yashsrivastava457 <125710366+yashsrivastava457@users.noreply.github.com>
2026-09-06 19:19:00 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,24 @@
 import sys
 from pathlib import Path
 
-BACKEND_DIR = Path(__file__).resolve().parent.parent / "backend"
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+ROOT_DIR = Path(__file__).resolve().parent.parent
+BACKEND_DIR = ROOT_DIR / "backend"
+
 sys.path.insert(0, str(BACKEND_DIR))
 
 from main import app
+
+
+@app.get("/", include_in_schema=False)
+def home():
+    return FileResponse(ROOT_DIR / "home.html")
+
+
+app.mount(
+    "/",
+    StaticFiles(directory=ROOT_DIR, html=True),
+    name="frontend"
+)
